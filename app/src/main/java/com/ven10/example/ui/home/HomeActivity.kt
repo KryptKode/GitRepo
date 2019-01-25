f90320728d8756fe32a65a7c70ea2b2ba7fd9561
KryptKode <paulakhamiogu@gmail.com>
2019-01-25 01:51:08 +0100

    Changed action bar title

--- app/src/main/java/com/ven10/example/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ven10/example/ui/home/HomeActivity.kt
@@ -1,11 +1,13 @@
 package com.ven10.example.ui.home
 
+import com.ven10.example.R
 import com.ven10.example.ui.base.BaseFragmentActivity
 import javax.inject.Inject
 
 class HomeActivity : BaseFragmentActivity<HomeFragment>() {
     @Inject lateinit var fragment: HomeFragment
     override fun accessFragment(): HomeFragment {
+        supportActionBar?.title = getString(R.string.trending_repos)
         return fragment
     }
 
